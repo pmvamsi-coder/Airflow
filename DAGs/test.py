3f244c82f8eb5fb1266f3c91320864ad19599cf4
pmvamsi-coder <pmohanvamsi2@hotmail.com>
2021-04-04 09:40:14 +0530

    test1

---
+++ DAGs/test.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+# Initialising the DAG object
+
+dag = DAG(
+    dag_id="test1",
+    start_date=airflow.utils.dates.days_ago(1),
+    schedule_interval=None,
+)
+
+download_launches = BashOperator(
+    task_id="print date",
+    bash_commands="Hellow world",
+    dag=dag,
+)
