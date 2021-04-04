61ffcb1ff583d38823e212672f2c97f925a41dae
pmvamsi-coder <pmohanvamsi2@hotmail.com>
2021-04-04 15:06:31 +0530

    reset_DAG

---
+++ DAGs/reset_DAG.py
@@ -0,0 +1,33 @@
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+# Initialising the DAG object
+
+dag = DAG(
+    dag_id="Reset_DAG",
+    start_date=airflow.utils.dates.days_ago(1),
+    schedule_interval=None,
+)
+
+t1_cmd = """
+rm -rf /home/vamsi/Airflow"""
+
+t1 = BashOperator(
+    task_id="DeleteFiles",
+    bash_command=t1_cmd,
+    dag=dag,
+)
+
+cmd = """
+git clone https://github.com/pmvamsi-coder/Airflow.git /home/vamsi/Airflow/
+"""
+
+t2 = BashOperator(
+    task_id="print",
+    bash_command=cmd,
+    dag=dag,
+)
+
+t1 >> t2
